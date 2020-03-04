3bec2ed3b8103eac322b01cf3fc7cfc8707c8581
Selina <>
2020-03-04 15:13:44 +0800

    新增项目

---
+++ SSLiveStreaming/SSLiveStreaming/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SSLiveStreaming
+//
+//  Created by Selina on 2020/3/4.
+//  Copyright © 2020 Selina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
